3feb8866057cbbcc711100de768b9181a264bb8d
Tayyaba <tayyaba.afzal@gmail.com>
2024-03-27 16:01:44 +0000

    array created to host all project info. Functions added to show tite and descriptio o the page

---
+++ src/components/projects/Projects.jsx
@@ -0,0 +1,92 @@
+import React from 'react';
+import schedular from '../../assets/ss2.png';
+import wd from '../../assets/1.png';
+import bx from '../../assets/home-page.png';
+import dp from '../../assets/1.gif';
+import jf from '../../assets/jf.png';
+import pg from '../../assets/5.png';
+import './project.css';
+
+const projects = [
+  {
+
+    title: "BrumExplorer",
+    description: "HTML, CSS, JavaScript, API",
+    link: "https://github.com/sunielmudhar/BrumExplorer",
+    deployedLink: "https://github.com/sunielmudhar/BrumExplorer",
+    image: bx,
+  },
+  {
+    title: "Jobify",
+    description: "HTML, CSS, JavaScript, API, Tailwind, Animate.CSS",
+    link: "https://github.com/victoriadeyemi/jobify-project",
+    deployedLink: "https://github.com/victoriadeyemi/jobify-project",
+    image: jf
+  },
+
+  
+  {
+    title: "Daily Planner App",
+    description: "HTML, CSS, JavaScript",
+    link: "https://github.com/tayyabaifzal/Daily-Planner-App",
+    deployedLink: "https://tayyabaifzal.github.io/Daily-Planner-App/",
+    image: dp
+  },
+ 
+  {
+    title: "Weather Dashboard",
+    description: "HTML, CSS, JavaScript, API",
+    link: "https://github.com/tayyabaifzal/Weather-Dashboard",
+    deployedLink: "https://tayyabaifzal.github.io/Weather-Dashboard/",
+    image: wd,
+  },
+ 
+
+  {
+    title: "Password Generator",
+    description: "HTML, CSS, JavaScript",
+    link: "https://github.com/tayyabaifzal/Password-Generator-",
+    deployedLink: "https://tayyabaifzal.github.io/Password-Generator-/",
+    image: pg
+  },
+  {
+    title: "Coding Quiz",
+    description: "HTML, CSS, JavaScript",
+    link: "https://github.com/tayyabaifzal/code-quiz",
+    deployedLink: "https://tayyabaifzal.github.io/code-quiz/",
+    image: schedular,
+  },
+
+];
+
+export default function Projects() {
+  return (
+
+    <div className="projects-container">
+      {projects.map((project, index) => (
+        <div key={index} className="project-card">
+          <div className="img-container">
+            <img className="project-img" src={project.image} alt={project.title} />
+          </div>
+          <div className="content">
+            <p>
+              <strong>Title:</strong> {project.title}
+            </p>
+            <p>
+              <strong>Repo:</strong> <a href={project.link}>GitHub</a>
+            </p>
+            <p>
+            <strong>Skills Used:</strong> {project.description}
+              {/* <strong>Skills used:</strong> <a href={project.description}></a> */}
+            </p>
+          </div>
+        </div>
+
+      ))
+      }
+
+    </div >
+  );
+}
+
+
