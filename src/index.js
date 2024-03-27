d5afc535ca427a6dd5b16a45a60857bae2641393
Tayyaba <tayyaba.afzal@gmail.com>
2024-03-27 15:48:41 +0000

    basic dependencies installed. folders and files for components created

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import ReactDOM from 'react-dom/client';
+import 'semantic-ui-css/semantic.min.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
